ed16415f56b6d65018affa9aaf74f896a30836a1
Demonware023 <Awamchimere@icloud.com>
2024-08-22 19:51:59 -0700

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,36 +6,30 @@
 # The data set can contain multiple characters
 # The data will be represented by a list of integers
 # Each integer represents 1 byte of data, therefore you only need to
-# handle the 8 least significant bits of each integer
+# handle the 8 least significant bits of each integer.
 
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
+    """
+    method that determines if a given data set
+    represents a valid UTF-8 encoding
+    """
+    nbytes = 0
 
-    # Masks to check the most significant bits
-    mask1 = 1 << 7
-    mask2 = 1 << 6
+    b1 = 1 << 7
+    b2 = 1 << 6
 
-    for num in data:
-        # Get the 8 least significant bits of the integer
-        byte = num & 0xFF
-
-        if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if (byte & mask1) == 0:
+    for i in data:
+        b = 1 << 7
+        if nbytes == 0:
+            while b & i:
+                nbytes += 1
+                b = b >> 1
+            if nbytes == 0:
                 continue
-            elif (byte & (mask1 >> 1)) == mask1:
-                num_bytes = 1
-            elif (byte & (mask1 >> 2)) == (mask1 >> 1):
-                num_bytes = 2
-            elif (byte & (mask1 >> 3)) == (mask1 >> 2):
-                num_bytes = 3
-            else:
+            if nbytes == 1 or nbytes > 4:
                 return False
         else:
-            # Check if the byte is a valid continuation byte
-            if not (byte & mask1 and not (byte & mask2)):
+            if not (i & b1 and not (i & b2)):
                 return False
-        num_bytes -= 1
-
-    return num_bytes == 0
+        nbytes -= 1
+    return nbytes == 0
